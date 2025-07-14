c684de4a9425c3261b55ea4e88348d10730eaf1c
GuguM22 <gugu.mkhwanazi40@gmail.com>
2025-07-14 15:02:34 +0200

    first commit

---
+++ src/main/java/com/In_need/inNeedApp/InNeedApplication.java
@@ -0,0 +1,13 @@
+package com.In_need.inNeedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InNeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InNeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/In_need/inNeedApp/InNeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.In_need.inNeedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InNeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
