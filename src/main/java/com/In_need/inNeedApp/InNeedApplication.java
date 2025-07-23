2f0112b295a541d5a38bdf7e3e6bc8d7032c59b8
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2025-07-23 12:24:23 +0200

    Added Dependecies, Properticies

--- src/main/java/com/In_need/inNeedApp/InNeedApplication.java
+++ src/main/java/com/In_need/inNeedApp/InNeedApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InNeedApplication {
+public class
+InNeedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InNeedApplication.class, args);
