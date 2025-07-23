cc89cd37c91577818213b7eb8dd89fbea261e1ec
Ngoakoana <ngoakoana.mphago460@gmail.com>
2025-07-23 14:52:12 +0200

    added Users class

--- src/main/java/com/In_need/inNeedApp/model/Users.java
+++ src/main/java/com/In_need/inNeedApp/model/Users.java
@@ -1,8 +1,70 @@
 package com.In_need.inNeedApp.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.management.relation.Role;
+import java.util.Objects;
 
+@Data
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "verification_token")
+    private String verificationToken;
+
+    @Column(name = "verified")
+    private boolean verified = false;
+
+    @Column(name = "reset_token")
+    private String resetToken;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users user = (Users) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", email='" + email + " " +
+        ", name='" + name + " " +
+        ", verified=" + verified +
+                ", role=" + role +
+                '}';
+    }
 }
